369188a05b340c4831f28e105795f9b9e71f1692
Ahmad Elassuty <ahmad.elassuty@gmail.com>
2020-08-19 22:00:28 +0200

    Add base Configuration class with the delivery options (#1)
    * add gem base configuration class
    
    * add EventRouter basic specs
    
    * add specs for base Configuration class

---
+++ lib/event_router/errors/unsupported_option_error.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative "../error"
+
+module EventRouter
+  module Errors
+    class UnsupportedOptionError < Error
+      def initialize(message: nil, config:, option:, supported_options:)
+        @config             = config
+        @option             = option
+        @supported_options  = supported_options
+
+        super(message || self.message)
+      end
+
+      def message
+        "Unsupported #{@option} for #{@config} configuration." +
+          " Currently supports #{@supported_options}"
+      end
+    end
+  end
+end
